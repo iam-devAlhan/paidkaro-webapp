cc5ae6624add7c6ddb2940904ed0e1309016d42c
iam-devAlhan <Alhaanahmed123@gmail.com>
2025-05-05 17:37:16 +0500

    "Review section and footer left of the landing page"

--- client/src/ui/components/landing/landing_page.tsx
+++ client/src/ui/components/landing/landing_page.tsx
@@ -1,15 +1,17 @@
 import Navbar from "../landing/navbar";
 import AboutSection from "./about_section";
 import BrandSection from "./brand_section";
+import SkillSection from "./skill_section";
 
 export default function LandingPage() {
-    return (
-        <>
-            <Navbar />
-            <main>
-                <BrandSection />
-                <AboutSection />
-            </main>
-        </>
-    )
+  return (
+    <>
+      <Navbar />
+      <main>
+        <BrandSection />
+        <AboutSection />
+        <SkillSection />
+      </main>
+    </>
+  );
 }
