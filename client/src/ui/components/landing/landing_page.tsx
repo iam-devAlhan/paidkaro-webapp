300bde5d74f4ede0e6256ecfb1199b107fb53bc7
iam-devAlhan <Alhaanahmed123@gmail.com>
2025-05-07 11:56:15 +0500

    Updated, Landing Page completed only responsiveness left'

--- client/src/ui/components/landing/landing_page.tsx
+++ client/src/ui/components/landing/landing_page.tsx
@@ -1,17 +1,23 @@
 import Navbar from "../landing/navbar";
 import AboutSection from "./about_section";
 import BrandSection from "./brand_section";
+import Footer from "./footer";
+import ReviewSection from "./review_section";
 import SkillSection from "./skill_section";
 
 export default function LandingPage() {
   return (
     <>
-      <Navbar />
+      <header><Navbar /></header>
       <main>
         <BrandSection />
         <AboutSection />
         <SkillSection />
+        <ReviewSection />
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
