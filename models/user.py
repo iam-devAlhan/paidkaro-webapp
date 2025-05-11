8a1447f56fab6e1ea29bc8bd537b96bbf0433f7a
iam-devAlhan <Alhaanahmed123@gmail.com>
2025-05-12 02:14:34 +0500

    Defined SQL Model through SQLAlchemy

--- models/user.py
+++ models/user.py
@@ -3,6 +3,14 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 class User(Base):
-    pass
+    __tablename__ = "Users"
+
+    u_id = Column(Integer, primary_key=True)
+    u_name = Column(String(50), nullable=False)
+    u_email = Column(String(50), nullable=False, unique=True)
+    firebase_uid = Column(String(255), unique=True, nullable=False)
+    org_joinedID = Column(Integer)
+    profile_picurl = Column(String(255))
+    description = Column(String(255))
 
 
