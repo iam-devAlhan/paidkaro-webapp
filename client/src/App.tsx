9a542bd0495eed845581d092c00d81a5c0f2d073
iam-devAlhan <Alhaanahmed123@gmail.com>
2025-05-12 02:16:03 +0500

    Added frontend routes using react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,22 @@ import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import LandingPage from './ui/components/landing/landing_page'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthComponent from './ui/components/auth/AuthComponent';
+import AuthLoginComponent from './ui/components/auth/AuthLoginComponent';
+
 function App() {
  
 
   return (
     <>
-      <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path='/auth_signup' element={<AuthComponent />} />
+          <Route path="/auth_login" element={<AuthLoginComponent />} /> 
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
