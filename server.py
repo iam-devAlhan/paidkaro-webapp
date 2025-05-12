2c60508152782145001dcf65328e82cddbc67b68
iam-devAlhan <Alhaanahmed123@gmail.com>
2025-05-12 11:04:36 +0500

    Added Base import to create mysql tables automatically

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from routes.auth_router import router as auth_router
+from database import engine, Base
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="PaidKaro Application", description="Created and Developed using FastAPI"
